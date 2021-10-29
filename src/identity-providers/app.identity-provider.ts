83335ef8215ccaad997ca0857875398cebce6cbd
mathieugh <mathieu.girouxh@gmail.com>
2021-10-29 14:30:39 -0400

    - Add comment for identity provider

--- src/identity-providers/app.identity-provider.ts
+++ src/identity-providers/app.identity-provider.ts
@@ -5,6 +5,14 @@ import { AppModuleKeyname } from "../app.module";
 import { LogHandlerInterface } from "@pristine-ts/logging";
 import { UserIdentityInterface } from "../interfaces/user-identity.interface";
 
+/**
+ * The app identity provider is a way of extending the base Identity Provider in Pristine.
+ * This way, we can easily type the identity with a new interface that is specific to our app (in this case UserIdentityInterface).
+ * AppIdentityProvider gives us the chance to map the decoded identity to our type.
+ *
+ * Don't forget to add the tag ServiceDefinitionTagEnum.IdentityProvider, so that our provider gets called automatically when
+ * Pristine receives a request !
+ */
 @tag(ServiceDefinitionTagEnum.IdentityProvider)
 @injectable()
 export class AppIdentityProvider implements IdentityProviderInterface {
